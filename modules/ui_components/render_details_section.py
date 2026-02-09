43f83732b2f8473fc178d065e3078257ee101580
Francisco Egea <egeafran02@gmail.com>
2026-02-09 12:32:14 -0300

    fix datetime

--- modules/ui_components/render_details_section.py
+++ modules/ui_components/render_details_section.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from datetime import date
+import pandas as pd
 from modules.utils.exporters import dataframe_to_excel_bytes
 from modules.utils.app_utils import ordenar_maquinas_personalizado
 def render_details_section(schedule, detalle_maquina, df, cfg=None): # Added cfg param
